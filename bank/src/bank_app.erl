265e7ff7bc45c71b4274ddd8ee405d4436934156
Bryan Hunter <bryan.hunter@fireflylogic.com>
2014-06-04 04:07:43 +0200

    Replaced gproc

--- bank/src/bank_app.erl
+++ bank/src/bank_app.erl
@@ -5,15 +5,13 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-
 %% Application callbacks
 
 start(_StartType, _StartArgs) ->
-	ensure_started(gproc),
 	bank_event_store:init(),
     bank_read_store:init(),
 	keypid:init(),
-	
+
     case bank_sup:start_link() of
         {ok, Pid} ->
             bank_command_handler:add_handler(),
@@ -25,14 +23,3 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
-
-
-%% Private functions
-
-ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
